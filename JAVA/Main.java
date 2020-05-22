5491a7c26e4d589d51bf1f71ea16217591789212
iamm3chanic <64973921+iamm3chanic@users.noreply.github.com>
2020-05-22 20:24:28 +0300

    Add files via upload

---
+++ JAVA/Main.java
@@ -0,0 +1,203 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[1000];
+        mix(array);
+        print(array);
+        bubbleSort(array);
+        print(array);
+
+        mix(array);
+        insertionSort(array);
+        print(array);
+
+        mix(array);
+        selectionSort(array);
+        print(array);
+
+        mix(array);
+        mergeSort(array,0,999);
+        print(array);
+
+        mix(array);
+        heapSort(array);
+        print(array);
+
+        mix(array);
+        quickSort(array,0,999);
+        print(array);
+    }
+
+    public static void mix(int[] array) {
+        for(int i=0;i<1000;i++) {
+            Random random = new Random();
+            int k=random.nextInt(2000);
+            array[i]=k;
+        }
+    }
+
+    public static void print(int[] array) {
+        for(int i=0;i<1000;i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("--------------------------");
+    }
+
+    public static void bubbleSort(int[] array) {
+        boolean sorted = false;
+        int tmp;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i+1]) {
+                    tmp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = tmp;
+                    sorted = false;
+                }
+            }
+        }
+    }
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while(j >= 0 && current < array[j]) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
+        }
+    }
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+        }
+    }
+
+    public static void mergeSort(int[] array, int left, int right) {
+        if (right <= left) return;
+        int mid = (left+right)/2;
+        mergeSort(array, left, mid);
+        mergeSort(array, mid+1, right);
+        merge(array, left, mid, right);
+    }
+    static void merge(int[] array, int left, int mid, int right) {
+        // вычисляем длину
+        int lengthLeft = mid - left + 1;
+        int lengthRight = right - mid;
+
+        // создаем временные подмассивы
+        int[] leftArray = new int [lengthLeft];
+        int[] rightArray = new int [lengthRight];
+
+        // копируем отсортированные массивы во временные
+        System.arraycopy(array, left + 0, leftArray, 0, lengthLeft);
+        for (int i = 0; i < lengthRight; i++)
+            rightArray[i] = array[mid+i+1];
+
+        // итераторы содержат текущий индекс временного подмассива
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        // копируем из leftArray и rightArray обратно в массив
+        for (int i = left; i < right + 1; i++) {
+            // если остаются нескопированные элементы в R и L, копируем минимальный
+            if (leftIndex < lengthLeft && rightIndex < lengthRight) {
+                if (leftArray[leftIndex] < rightArray[rightIndex]) {
+                    array[i] = leftArray[leftIndex];
+                    leftIndex++;
+                }
+                else {
+                    array[i] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+            }
+            // если все элементы были скопированы из rightArray, скопировать остальные из leftArray
+            else if (leftIndex < lengthLeft) {
+                array[i] = leftArray[leftIndex];
+                leftIndex++;
+            }
+            // если все элементы были скопированы из leftArray, скопировать остальные из rightArray
+            else if (rightIndex < lengthRight) {
+                array[i] = rightArray[rightIndex];
+                rightIndex++;
+            }
+        }
+    }
+    static void heapify(int[] array, int length, int i) {
+        int leftChild = 2*i+1;
+        int rightChild = 2*i+2;
+        int largest = i;
+
+        if (leftChild < length && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < length && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        // if have to substitute
+        if (largest != i) {
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+            heapify(array, length, largest);
+        }
+    }
+
+    public static void heapSort(int[] array) {
+        if (array.length == 0) return;
+
+        // make heap
+        int length = array.length;
+        for (int i = length / 2-1; i >= 0; i--)
+            heapify(array, length, i);
+
+        for (int i = length-1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+    static int partition(int[] array, int begin, int end) {
+        int pivot = end;
+
+        int counter = begin;
+        for (int i = begin; i < end; i++) {
+            if (array[i] < array[pivot]) {
+                int temp = array[counter];
+                array[counter] = array[i];
+                array[i] = temp;
+                counter++;
+            }
+        }
+        int temp = array[pivot];
+        array[pivot] = array[counter];
+        array[counter] = temp;
+
+        return counter;
+    }
+
+    public static void quickSort(int[] array, int begin, int end) {
+        if (end <= begin) return;
+        int pivot = partition(array, begin, end);
+        quickSort(array, begin, pivot-1);
+        quickSort(array, pivot+1, end);
+    }
+}
